fdd6cdd3d9916e4309aa8c01767983b4c96ed48d
darshitbhalodilampros <darshitbhalodia@lmapros.tech>
2024-06-05 11:32:03 +0530

    new config file except craco and other folder except src in web

--- apps/web/functions/utils/getNetworkLogoURL.ts
+++ apps/web/functions/utils/getNetworkLogoURL.ts
@@ -1,16 +1,16 @@
-import { Chain } from 'uniswap/src/data/graphql/uniswap-data-api/__generated__/types-and-hooks'
+import { Chain } from "uniswap/src/data/graphql/uniswap-data-api/__generated__/types-and-hooks";
 
 export default function getNetworkLogoUrl(network: string, origin: string) {
   switch (network) {
     case Chain.Polygon:
-      return origin + '/images/logos/Polygon_Logo.png'
+      return origin + "/images/logos/Polygon_Logo.png";
+    case Chain.Mode:
+      return origin + "/images/logos/mode.svg";
     case Chain.Arbitrum:
-      return origin + '/images/logos/Arbitrum_Logo.png'
+      return origin + "/images/logos/Arbitrum_Logo.png";
     case Chain.Optimism:
-      return origin + '/images/logos/Optimism_Logo.png'
-    case Chain.Celo:
-      return origin + '/images/logos/Celo_Logo.png'
+      return origin + "/images/logos/Optimism_Logo.png";
     default:
-      return ''
+      return "";
   }
 }
